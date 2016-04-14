7cc82e5bbf1523993deeb44b723aed0156bbbaca
Simon Østergaard Kristensen <sok@sekoia.dk>
2016-04-14 15:34:18 +0200

    Caliburn Micro added as MVVM framework

---
+++ Graphikos/Graphikos/ViewModels/ShellViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphikos.ViewModels
+{
+   
+    public class ShellViewModel
+    {
+        public string SomeMessage { get; set; }
+        public ShellViewModel()
+        {
+            SomeMessage = "hello world";
+        }
+    }
+}
