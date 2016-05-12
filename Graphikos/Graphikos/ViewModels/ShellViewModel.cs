99949c0b28537945b4dd8bbaffc2c7fe914373fa
Rotvig <renerotvig@msn.com>
2016-05-12 20:10:49 +0200

    Bitmap added

--- Graphikos/Graphikos/ViewModels/ShellViewModel.cs
+++ Graphikos/Graphikos/ViewModels/ShellViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using System.Drawing;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Windows.Media;
+using System.Windows;
+using System.Windows.Media.Imaging;
 using Caliburn.Micro;
 using Graphikos.Models;
 using Graphikos.Scheme;
@@ -15,15 +16,21 @@ namespace Graphikos.ViewModels
         private readonly ISchemeHandler _schemeHandler;
         private string _input = "";
         private string _someMessage = "";
-        public ObservableCollection<Point> Points { get; set; }
+        private BitmapSource _imageSource;
+        private Bitmap operableBitMap;
+
 
         public ShellViewModel(ISchemeHandler schemeHandler)
         {
             if (schemeHandler == null)
                 throw new ArgumentNullException(nameof(schemeHandler));
-
             _schemeHandler = schemeHandler;
-            Points = new ObservableCollection<Point>();
+        }
+
+        public BitmapSource ImageSource
+        {
+            get { return _imageSource; }
+            set { _imageSource = value; NotifyOfPropertyChange(() => ImageSource); }
         }
 
         public string Input
@@ -39,7 +46,7 @@ namespace Graphikos.ViewModels
 
         public void Evaluate()
         {
-            Points.Clear();
+            operableBitMap = new Bitmap(400, 400);
             foreach (var expressionToEvaluate in GetExpressionsToEvaluate(_input))
             {
                 var result = _schemeHandler.CallSchemeFunc(expressionToEvaluate);
@@ -49,30 +56,33 @@ namespace Graphikos.ViewModels
                 var enumerable = result.Select(Convert.ToDouble);
                 var listOfCoordinates = enumerable.ToList();
                 Console.WriteLine("Algorithm started");
-                AddCoordinatesToCanvas(listOfCoordinates);
+                SetPixels(listOfCoordinates);
             }
             
         }
 
-        public void AddCoordinatesToCanvas(IReadOnlyCollection<double> listOfCoordinates)
-        {
-
-            for (var i = 0; i + 3 < listOfCoordinates.Count; i++)
-            {
-                var point = new Point
-                {
-                    X = listOfCoordinates.ElementAt(i),
-                    Y = listOfCoordinates.ElementAt(i + 1),
-                    Color = new SolidColorBrush((Color)ColorConverter.ConvertFromString(EnumDescriptions.GetEnumDescription(GraphikosColors.Red)))
-                };
-                i++;
-                Points.Add(point);
-            }
-                Console.WriteLine("Algorithm ended");
-        }
         public IEnumerable<string> GetExpressionsToEvaluate(string input)
         {
             return Regex.Split(input, "\r\n").Where(x => !string.IsNullOrWhiteSpace(x));
+        }
+
+        public void SetPixels(IReadOnlyCollection<double> listOfCoordinates)
+        {          
+            for (var i = 0; i + 3 < listOfCoordinates.Count; i++)
+            {
+                operableBitMap.SetPixel((int)listOfCoordinates.ElementAt(i), (int)listOfCoordinates.ElementAt(i + 1), ColorTranslator.FromHtml(EnumDescriptions.GetEnumDescription(GraphikosColors.Red)));
+            }
+
+            ImageSource = BitmapToBitmapSource(operableBitMap);
+        }
+
+        public static BitmapSource BitmapToBitmapSource(Bitmap source)
+        {
+            return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                          source.GetHbitmap(),
+                          IntPtr.Zero,
+                          Int32Rect.Empty,
+                          BitmapSizeOptions.FromEmptyOptions());
         }
     }
 }
--- Graphikos/GraphikosTests/ViewModels/ShellViewModelTests.cs
+++ Graphikos/GraphikosTests/ViewModels/ShellViewModelTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Graphikos.Scheme;
 using Graphikos.ViewModels;
 using IronScheme.Runtime;
@@ -23,20 +22,9 @@ namespace GraphikosTests.ViewModels
         [Fact]
         public void CanGetExpressionToEvaluate()
         {
-            var testData = new List<string>() { "line(11, 11, 2, 213)", "line(23, 23, 23, 23)" };
+            var testData = new List<string> { "line(11, 11, 2, 213)", "line(23, 23, 23, 23)" };
             var result = shellViewModel.GetExpressionsToEvaluate("line(11, 11, 2, 213)\r\nline(23, 23, 23, 23)");
             result.ShouldBe(testData);
-        }
-
-        [Fact]
-        public void CanAddCoordinatesToCanvas()
-        {
-            var testData = new List<double>() {2, 5, 3, 5, 4, 5, 5,5};
-            shellViewModel.AddCoordinatesToCanvas(testData);
-            shellViewModel.Points.Count.ShouldBe(3);
-            shellViewModel.Points.Count(x => x.X == 2 && x.Y == 5).ShouldBe(1);
-            shellViewModel.Points.Count(x => x.X == 3 && x.Y == 5).ShouldBe(1);
-            shellViewModel.Points.Count(x => x.X == 4 && x.Y == 5).ShouldBe(1);
         }
 
         [Fact]
